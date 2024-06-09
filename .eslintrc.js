69f0c71faa47d3c29649cb4a46b5df611aba1b9f
Xintre <natalia.rozga02@gmail.com>
2024-06-09 17:45:01 +0200

    ci: added CI to run eslint on the cloud

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'prettier', 'plugin:react/recommended'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
